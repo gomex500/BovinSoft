d7994d5e8a1e4571c17fae95d6f6da71ef912241
gomex500 <gomezfreddy886@gmail.com>
2024-10-11 22:32:08 -0600

    creacion de la vista login

--- App.jsx
+++ App.jsx
@@ -1,9 +1,10 @@
 import Navegacion from './components/Navegacion';
 import Splash from './components/Splash.jsx';
-
+import Login from './views/Login';
 export default function App() {
   return (
-    <Navegacion/>
+    // <Navegacion/>
     // <Splash/>
+    <Login/>
   );
 }
---
+++ views/Login.jsx
@@ -0,0 +1,126 @@
+import React, {useState} from 'react';
+import { StyleSheet, View, Text, Image, TextInput, Button, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import * as Font from 'expo-font';
+
+const Login = () => {
+    
+    const [ver, setVer] = useState(true);
+
+    const verPassword = () =>{
+        if (ver) {
+            setVer(false);
+        } else {
+            setVer(true);
+        }
+    }
+
+    return (
+        <KeyboardAvoidingView 
+            style={styles.container}
+            behavior="height"
+        >
+            <Image
+                source={require('../assets/img/Logo.png')} 
+                style={styles.logo}
+            />
+            <Text
+                style={styles.title}
+            >Inicio de Sesion</Text>
+            <View style={styles.contLogin}>
+                <View>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Correo"
+                        keyboardType="email-address"
+                    />
+                    <Icon
+                        name="user"
+                        size={25}
+                        style={styles.icon}
+                    />
+                </View>
+                <View>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Password"
+                        secureTextEntry={ver}
+                    />
+                    <Icon
+                        name="lock"
+                        size={25}
+                        style={styles.icon}
+                        onPress={verPassword}
+                    />
+                </View>
+                <TouchableOpacity style={styles.btn}>
+                    <Text style={styles.btnText}>Iniciar</Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#f1f1f1',
+        alignItems: 'center',
+        flex: 1,
+    },
+    logo:{
+        width: 200,
+        height: 110,
+        marginTop: 150
+    },
+    title:{
+        fontSize: 32,
+        fontWeight: '900',
+        marginTop:100,
+        color: '#1B4725'
+    },
+    contLogin:{
+        flex: 1,
+        width:'90%',
+        padding:40,
+        alignItems: 'center',
+    },
+    input:{
+        width:300,
+        height: 50,
+        color:'#252525',
+        paddingVertical: 10,
+        paddingLeft: 40,
+        marginTop: 20,
+        borderRadius:8,
+        borderColor:'#1B4725',
+        borderWidth: 2,
+        borderTopWidth:0,
+        borderLeftWidth:0,
+        borderRightWidth:0,
+        fontSize:18,
+        color:'#1B4725',
+        fontWeight: 'bold'
+    },
+    icon: {
+       position: 'absolute',
+       left: 10,
+       top: 32,
+       zIndex: 1,
+       color: '#1B4725'
+    },
+    btn:{
+        backgroundColor:'#1B4725',
+        width:"100%",
+        height: 50,
+        marginTop:50,
+        borderRadius:10,
+        paddingTop:12
+    },
+    btnText:{
+        color: '#f2f2f2',
+        textAlign:'center',
+        fontSize:20
+    }
+});
+
+export default Login;
